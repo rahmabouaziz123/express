23530fe637b2546761d1a4299450ad145ef3a09d
rahmabouaziz123 <bouazizrahma5@gmail.com>
2022-10-26 12:46:39 +0200

    commit

---
+++ index.js
@@ -0,0 +1,71 @@
+
+const express=require("express");
+const app=express();
+const path=require("path");
+// const users=require("./api/list")
+const produits=require("././public/api/list")
+
+
+// app.get("/",(req,res)=>res.send("<h1> hello every body</h1>"))
+// app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"public","index.html")))
+
+
+app.use(date=(req,res,next)=>{
+
+  var objDate = new Date();
+  var hours = objDate.getHours();
+  var day =objDate.getDay();
+  if((hours >= 9 && hours <= 17)&&(day!=0 || day!=6)){
+    next()
+
+  }
+  else{
+      res.send('We Are Closed');
+
+      
+  }
+})
+
+
+app.use(express.static(path.join(__dirname,"public")))
+
+app.get("/produit/:id",(req,res)=>res.json(produits.find(el=>el.id==req.params.id)))
+
+
+
+
+
+
+app.get("/",(req,res)=>res.sendFile(path.join(__dirname,"public","home.html")))
+app.get("/contact",(req,res)=>res.sendFile(path.join(__dirname,"public","contact.html")))
+app.get("/service",(req,res)=>res.sendFile(path.join(__dirname,"public","Our Services.html")))
+
+
+
+// app.get('/now', function(req, res, next){
+//     req.time = new Date().toString();
+//     next();
+//   },
+//     function(req, res) {
+//       res.send({"time": req.time});
+      
+//     }  
+  
+//       );
+
+
+
+
+
+
+  
+
+
+
+
+
+
+
+
+const PORT=process.env.PORT||3000
+app.listen(PORT,err=>err?console.log(err):console.log(`server is run sucessfuly rahma on port ${PORT}`))
---
+++ public/api/list.js
@@ -0,0 +1,26 @@
+const produit = [
+  {
+    id: 1,
+    name: "Pizza Margherita",
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ4Zyu-T5WQ7bLxROPZPWo7u65WqNKpnk7NWQ&usqp=CAU",
+    dex: ` Impossible de vous parler de pizza sans évoquer l’une des plus
+    anciennes et plus célèbres spécialités napolitaines, La
+    Margherita`,
+  },
+
+  {
+    id: 2,
+    name: "Pizza Reine",
+    img: "https://cuisinedecheznous.net/wp-content/uploads/2021/10/pizza2.jpg",
+    dex: " Voici l’une des pizzas préférées des Français et des Européens en général. La pizza Reine ou Regina n’est autre qu’une Margherita à laquelle sont ajoutés des tranches ",
+  },
+
+
+  { id: 3, name: "", img: "", dex: "" },
+  { id: 3, name: "", img: "", dex: "" },
+  { id: 3, name: "", img: "", dex: "" },
+  { id: 3, name: "", img: "", dex: "" },
+  { id: 3, name: "", img: "", dex: "" },
+];
+
+module.exports = produit;
